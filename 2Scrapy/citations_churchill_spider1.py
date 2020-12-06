9d5b7c0598c0c5d8bf2f97911b667e93c4ee5c29
mmadidan <nadjime.mmadidaroueche@edu.esiee.fr>
2020-12-07 00:52:38 +0100

    Exercice

--- 2Scrapy/citations_churchill_spider1.py
+++ 2Scrapy/citations_churchill_spider1.py
@@ -7,4 +7,8 @@ class ChurchillQuotesSpider(scrapy.Spider):
     def parse(self, response):
         for cit in response.xpath('//div[@class="figsco__quote__text"]'):
             text_value = cit.xpath('a/text()').extract_first()
-            yield { 'text' : text_value }
+            text_val = text_value.replace('“','',)
+            text_val = text_val.replace('”','',)
+            auth = response.xpath('//div[@class="figsco__fake__col-9"]')
+            author = auth.xpath('a/text()').extract_first()
+            yield { 'text' : text_val, 'auteur' : author}
